e0e9bad8c90167e838a1eaf1822b59a3cc103eb3
ThomasDelamarre <thomas.delamarre@student.ecp.fr>
2020-03-05 12:05:44 +0100

    Remove RefreshTokenActivity replaced by an Interface in the Auth Manager

--- app/src/main/java/com/example/thomas/stravaappwidgetextended/api/authenticator/AuthManager.java
+++ app/src/main/java/com/example/thomas/stravaappwidgetextended/api/authenticator/AuthManager.java
@@ -4,8 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.example.thomas.stravaappwidgetextended.Constants;
 import com.example.thomas.stravaappwidgetextended.api.pojo.AuthTokens;
 import com.example.thomas.stravaappwidgetextended.sharedPreferences.SharedPrefManager;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class AuthManager {
 
@@ -17,8 +23,21 @@ public class AuthManager {
         shared_pref_manager = new SharedPrefManager(this.context);
     }
 
+    TokenRefresher tokenRefresher = new TokenRefresher() {
+        @Override
+        public void success(Response<AuthTokens> response) {
+            AuthTokens authTokens =  response.body();
+            shared_pref_manager.saveAuthToken(authTokens);
+        }
+
+        @Override
+        public void failed(String message) {
+        }
+    };
+
     public AuthTokens getAuthTokens(){
-        refreshAuthTokens();
+
+        refreshAuthTokens(tokenRefresher);
         AuthTokens auth_token = new AuthTokens();
         auth_token.setExpiresAt(shared_pref_manager.getExpiresAt());
         auth_token.setAccessToken(shared_pref_manager.getAccessToken());
@@ -26,13 +45,35 @@ public class AuthManager {
         return auth_token;
     }
 
-    private void refreshAuthTokens(){
-        int expires_at = this.shared_pref_manager.getExpiresAt();
-        if (isExpired(expires_at)){
-            Intent intent = new Intent(this.context, RefreshTokenActivity.class);
-            this.context.startActivity(intent);
-            Log.i("Auth tokens", "refreshed");
 
+    private void refreshAuthTokens(final TokenRefresher token_refresher){
+        int expires_at = this.shared_pref_manager.getExpiresAt();
+        String refresh_token = this.shared_pref_manager.getRefreshToken();
+
+        if (isExpired(expires_at)){
+            String client_id = Constants.CLIENT_ID;
+            String client_secret = Constants.CLIENT_SECRET;
+            String grant_type = Constants.GRANT_TYPE_REFRESH;
+
+            Retrofit retrofit = AuthentificatorNetworkClient.getRetrofitClient();
+            Authenticator authenticator = retrofit.create(Authenticator.class);
+
+            Call call = authenticator.refreshTokens(client_id, client_secret, grant_type, refresh_token);
+            call.enqueue(new Callback() {
+                             @Override
+                             public void onResponse(Call call, Response response) {
+                                 Log.i("Token refresh", "success");
+                                 AuthTokens authTokens = (AuthTokens) response.body();
+                                 shared_pref_manager.saveAuthToken(authTokens);
+                                 token_refresher.success(response);
+                             }
+
+                             @Override
+                             public void onFailure(Call call, Throwable t) {
+                                 Log.i("Token refresh", "fail");
+                                 token_refresher.failed("Failed");
+                             }
+                         });
         } else {
             Log.i("Auth tokens", "not expired");
         }
@@ -47,5 +88,10 @@ public class AuthManager {
         } else {
             return Boolean.TRUE;
         }
+    }
+
+    public interface TokenRefresher{
+        void success(Response<AuthTokens> response);
+        void failed(String message);
     }
 }
